b6612bf6d952f2ba29099a254e89d465dca074ad
taognt <84730001+taognt@users.noreply.github.com>
2023-04-11 19:39:08 +0200

    commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,19 @@ const App = () => {
       </header>
       <div className="App-images">
         <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
+        <audio src={trackUrls[1]} controls />
       </div>
       <div className="App-buttons"></div>
     </div>
   );
 };
+
+const trackUrls = [
+  'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
+  'https://p.scdn.co/mp3-preview/5a12483aa3b51331aba663131dbac967ccb33d99',
+  'https://p.scdn.co/mp3-preview/31f65b6a613010f22316c7be335b62226cf2f263',
+  'https://p.scdn.co/mp3-preview/0f6b8a3524ec410020457da4cdd7717f9addce2f',
+  'https://p.scdn.co/mp3-preview/ac28d1b0be285ed3bfd8e9fa5fad133776d7cf36',
+];
 
 export default App;
